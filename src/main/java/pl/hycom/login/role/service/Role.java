73ceabcf3486e5e40c6cd7c18780a70ccaee8c1a
jakub.jawor <jakub.jawor@hycom.pl>
2018-10-04 15:28:01 +0200

    create entity

---
+++ src/main/java/pl/hycom/login/role/service/Role.java
@@ -0,0 +1,24 @@
+package pl.hycom.login.role.service;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "role")
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role")
+    private String role;
+}
